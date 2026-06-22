ba291807341a523f83c4af145ccb3ebc8391916a
Pablo Marino <pmarino2013@gmail.com>
2026-06-22 13:17:00 -0300

    Practica de for y while

---
+++ js/07-for.js
@@ -0,0 +1,73 @@
+//for
+/*
+- declaramos un iterador (number)
+- definir la condición
+- controlar iteración (++, --)
+*/
+
+//sintaxis
+//-------------
+// for (let index = 0; index <= 10; index++) {
+//   //acciones que se van a realizar
+//   console.log(i);
+// }
+
+// tabla 2
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`2 x ${i} = ${2 * i}`);
+//   }
+
+//Tabla 2.0
+
+// let tabla = prompt("Ingrese el número de la tabla a calcular");
+
+// if (!tabla) {
+//   alert("❌El valor ingresado no es correcto");
+// } else {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(`${tabla} x ${i} = ${tabla * i}`);
+//   }
+// }
+
+//Tabla 3.0
+// while (tabla) {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(`${tabla} x ${i} = ${tabla * i}`);
+//   }
+//   console.log("------------");
+//   tabla = prompt("Ingrese el número de la tabla a calcular");
+// }
+
+//Buscar pares entre un min y un max
+
+let min = 1;
+let max = 30;
+
+for (let index = min; index < max; index++) {
+  if (index % 2 === 0) {
+    console.log(`El número ${index} es par`);
+  }
+}
+
+//contar la cantidad de vocales en una frase
+
+/*
+ALGORITMO
+----------
+- datos de entrada: Frase
+- Proceso: Recorrer cada caracter de la frase y lo comparamos con las vocales, si coincide las acumulamos.
+- Salida: Mensaje con la cantidad de vocales 
+*/
+
+let frase = prompt("Ingrese la frase");
+let vocales = "a,á,e,é,i,í,o,ó,u,ú";
+let contadorVocales = 0;
+
+for (let i = 0; i < frase.length; i++) {
+  for (let j = 0; j < vocales.length; j++) {
+    if (frase[i] === vocales[j]) {
+      contadorVocales++;
+    }
+  }
+}
+console.log(`Cantidad de vocales encontradas: ${contadorVocales}`);
---
+++ js/08-while.js
@@ -0,0 +1,111 @@
+//while
+// mientras...
+// while (condition) {
+
+// }
+
+//contador
+let contador = 1;
+while (contador <= 10) {
+  console.log(contador);
+  contador++;
+}
+
+//Tabla 3.0
+let tabla = prompt("Ingrese el número de la tabla a calcular");
+while (tabla) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${tabla} x ${i} = ${tabla * i}`);
+  }
+  console.log("------------");
+  tabla = prompt("Ingrese el número de otra tabla a calcular");
+}
+
+//Acceso con contraseña
+let password = "12345678";
+
+let passwordUser = prompt("Ingrese la contraseña");
+while (passwordUser != password) {
+  passwordUser = prompt(
+    `❌La contraseña ingresada no es correcta\nIngrese la contraseña nuevamente`,
+  );
+}
+alert("✅Acceso concedido");
+
+//Cargar nombres de alumnos
+let nombre = prompt("Ingresa el nombre del alumno");
+let listaNombres = "";
+while (nombre) {
+  listaNombres += nombre + " ";
+  nombre = prompt("Ingresa el siguiento nombre de alumno");
+}
+if (listaNombres) {
+  console.log(listaNombres);
+}
+
+/*
+Ejercicio 3: Cajero Automático
+Comenzar con:
+let saldo = 100000;
+Mientras el usuario no escriba:
+    salir
+preguntar cuánto dinero desea retirar.
+Si el monto es menor o igual al saldo:
+    Restarlo del saldo.
+Si es mayor:
+    Mostrar "Saldo insuficiente".
+Al finalizar mostrar el saldo restante.
+*/
+
+let saldo = 1000000;
+let opciones = prompt(
+  "Cuánto dinero desea retirar | Escriba 'Salir' para cerrar el programa",
+);
+// while (opciones !== "Salir") {
+//   let isNumber = Number(opciones);
+//   if (!isNaN(isNumber)) {
+//     if (isNumber < saldo) {
+//       saldo = saldo - isNumber;
+//     } else {
+//       alert("Saldo insuficiente");
+//     }
+//     opciones = prompt(
+//       "Quiere retirar otro monto? | Escriba 'Salir' para cerrar el programa",
+//     );
+//   } else {
+//     opciones = prompt(
+//       "Ingrese un monto en número | Escriba 'Salir' para cerrar el programa",
+//     );
+//   }
+// }
+// alert(`El saldo restante de la cuenta es $${saldo}`);
+while (opciones !== "Salir") {
+  let isNumber = Number(opciones);
+
+  switch (true) {
+    case isNumber < saldo:
+      saldo = saldo - isNumber;
+      console.log(isNumber);
+      opciones = prompt(
+        "Ingrese otro monto | Escriba 'Salir' para cerrar el programa",
+      );
+      break;
+    case isNumber > saldo:
+      alert("Saldo insuficiente");
+      console.log(isNumber);
+
+      opciones = prompt(
+        "Ingrese otro monto | Escriba 'Salir' para cerrar el programa",
+      );
+      break;
+
+    default:
+      console.log(isNumber);
+
+      opciones = prompt(
+        "Ingrese un monto en número | Escriba 'Salir' para cerrar el programa",
+      );
+      break;
+  }
+}
+alert(`El saldo restante de la cuenta es $${saldo}`);
